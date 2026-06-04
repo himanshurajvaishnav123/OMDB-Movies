364c21cb3de8f637954f068d4d721029b29c23da
himanshu <himanshurajvaishnav@gmail.com>
2026-06-04 23:38:35 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
-
-function Navbar({totalResults}) {
+function Navbar({ totalResults }) {
   return (
-    <div className="h-16 px-8 flex justify-between items-center rounded-lg bg-[#0f172a] ">
+    <div className="h-16 px-8 flex justify-between items-center rounded-lg bg-[#0f172a]">
+      
       <div>
-        <h1 className="font-bold text-[#e11d48] text-xl">🍿HRV Movies</h1>
-        
+        <h1 className="font-bold text-[#e11d48] text-xl">
+          🍿 HRV Movies
+        </h1>
+        <p className="text-xs text-gray-300">
+          Created by Himanshu Raj Vaishnav
+        </p>
       </div>
-     
 
       <div className="font-bold text-indigo-100 text-lg">
         Showing 10 out of {totalResults}
       </div>
+
     </div>
   );
 }
